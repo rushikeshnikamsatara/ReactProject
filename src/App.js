ba210fbe7ea1c906d54984224a3e3363bf5154f0
rushikesh nikam <rushikeshnikam6@gmail.com>
2020-01-26 21:15:42 +0530

    components part-1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
-
+import Menu from './component/MenuComponent';
 function App() {
   return (
-    <div className="App">
+    <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Added reactsrap Navbar and NavbarBrand  </NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
-  );
+     
+  );s
 }
 
 export default App;
