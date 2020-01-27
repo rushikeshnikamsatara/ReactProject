0f15ede11b549b3a4116b87ddb4de1fa1f146e1e
rushikesh nikam <rushikeshnikam6@gmail.com>
2020-01-27 09:57:58 +0530

    Assignment 1

--- src/component/DishDetailComponent.js
+++ src/component/DishDetailComponent.js
@@ -1,46 +1,38 @@
-import { React,Component } from "react";
+import React, { Component } from 'react'
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle } from 'reactstrap'
 
-class Dishdetail extends Component{
-
-
-constructor(props){
-
-super(props);
-this.state={
-selectedDish:null
-};
-
+export default class DishdetailComponent extends Component {
+    render() {
+        const renderComments = this.props.dish.comments.map((comment) => 
+            <ul className="list-unstyled">
+            <li>
+                {comment.comment}
+            </li>
+            <li>
+                {comment.author + " " + comment.date}
+            </li>
+            </ul>
+        )
+        console.log(this.props.dish)
+        return (
+            <div className="container">
+                <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                        <CardBody>
+                            <CardTitle>{this.props.dish.name}</CardTitle>
+                            <CardText>{this.props.dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <h2>Comments</h2>
+                    {renderComments}
+                </div>
+                </div>
+            </div>
+        )
+    }
 }
-render(){
-    const  dish=this.props.selectedDish.map(dish){
-if (dish != null)
-      return(
-          <Card>
-              <CardImg top src={dish.image} alt={dish.name} />
-              <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-              </CardBody>
-          </Card>
-      );
-  else
-      return(
-          <div></div>
-      );
-      }
-
-      return (
-      <div className="row">
-      <div  className="col-12 col-md-5 m-1">
-      {dish}
-      </div>
-    </div>
-      );
-
-}
-
-
-}
-export default Dishdetail;
